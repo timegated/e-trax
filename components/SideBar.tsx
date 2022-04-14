ff656804787df673b116d72215243d5c38306df3
timegated <beccdaniel@gmail.com>
2022-04-13 20:12:43 -0700

    Added first half of navigation menu for SideBar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -16,6 +16,25 @@ import {
   MdFavorite,
 } from 'react-icons/md';
 import Image from 'next/image';
+import NextLink from 'next/link';
+
+const navigation = [
+  {
+    name: 'Home',
+    icon: MdHome,
+    route: '/',
+  },
+  {
+    name: 'Search',
+    icon: MdSearch,
+    route: '/search',
+  },
+  {
+    name: 'Library',
+    icon: MdLibraryMusic,
+    route: '/library',
+  },
+];
 
 const SideBar = () => {
   return (
@@ -36,11 +55,34 @@ const SideBar = () => {
           marginBottom="20px"
           paddingX="20px"
         >
-          <Image 
+          <Image
             src="https://s3.amazonaws.com/random.web.assets/logo.svg"
             height={60}
             width={120}
           />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navigation.map(navItem => {
+              return (
+                <ListItem
+                  key={navItem.name}
+                  paddingX="20px"
+                  fontSize="16px"
+                >
+                  <LinkBox>
+                  {/* passHref passes the href to it's child component */}
+                    <NextLink href={navItem.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon as={navItem.icon} color="#FFFFFF" marginRight="20px" />
+                        {navItem.name}
+                      </LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              );
+            })}
+          </List>
         </Box>
       </Box>
     </Box>
