9749e2deb390c007430ce79a6624ed0c2ffe338f
timegated <beccdaniel@gmail.com>
2022-04-22 08:05:36 -0700

    Add logic for signup sls function

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -5,6 +5,41 @@ import cookie from 'cookie';
 import prisma from '../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+/**
+ * We need bcrypt for any sensitive information, this provides us with hashing functions to
+ * obfuscate anything we don't want getting out to the world
+ * Cookies for storing information about the user and sending that with requests
+ * 
+ * How do serverless functions execute?
+ * Kind of works like a callback - sls functions don't fire until events come in/happen
+ * Whenever someone hits the /signup route the function will get executed
+ * Can respond to anything.
+ */
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body; // we get this off of the request body
 
+  let user;
+
+  // might fail so we use try-catch to catch/respond to anything that goes wrong
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (error) {
+    res.status(401);
+    res.json({ error: 'User Already Exists' });
+  }
+
+  const token = jwt.sign({
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    'hello',
+    { expiresIn: '8h' },
+  );
 };
